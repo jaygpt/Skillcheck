3b93d16044776635213cee472135ec61965fcb0b
Jay <jaygpt@iitk.ac.in>
2019-06-07 21:48:29 +0530

    second commit

---
+++ ethereum/compile.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+
+const campaignPath = path.resolve(__dirname, 'Contracts', 'College.sol');
+
+const source = fs.readFileSync(campaignPath, 'utf8');
+//console.log(source);
+const output = solc.compile(source, 1).contracts;
+// use only contract property
+//console.log('s');
+fs.ensureDirSync(buildPath);
+// create build folder
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':','') + '.json'),
+    output[contract]
+  );
+}
